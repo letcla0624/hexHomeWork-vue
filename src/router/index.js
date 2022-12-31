3076d910184afbec30de9e700400824677b5e088
letcla0624 <letcla0624@yahoo.com.tw>
2022-12-31 22:42:13 +0800

    [change] router web hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import GetProducts from "@/views/week1/GetProducts.vue";
 import HomeView from "@/views/week2/HomeView.vue";
 import ProductsView from "@/views/week2/ProductsView.vue";
@@ -27,7 +27,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes,
   linkActiveClass: "active",
 });
