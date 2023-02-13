2789944cd3af620a35de632dc6aaa6d96114289a
letcla0624 <letcla0624@yahoo.com.tw>
2023-02-13 23:38:36 +0800

    [update] prod imgurl

--- src/views/week6/front/ProdView.vue
+++ src/views/week6/front/ProdView.vue
@@ -43,7 +43,22 @@ onMounted(() => {
   <div>
     <div class="row">
       <div class="col-sm-6">
-        <img class="img-fluid" :src="product.obj.imagesUrl" alt="" />
+        <img
+          class="img-fluid"
+          :src="product.obj.imageUrl"
+          :alt="product.obj.title"
+        />
+        <div
+          class="imgBox d-flex justify-content-center align-items-center my-3"
+        >
+          <img
+            v-for="(img, idx) in product.obj.imagesUrl"
+            :key="img"
+            :src="img"
+            :alt="`${product.obj.title}_${idx}`"
+            class="w-100 h-100"
+          />
+        </div>
       </div>
       <div class="col-sm-6">
         <span class="badge bg-primary rounded-pill">{{
@@ -92,3 +107,15 @@ onMounted(() => {
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.imgBox {
+  width: 100px;
+  height: 100px;
+  overflow: hidden;
+
+  img {
+    object-fit: cover;
+  }
+}
+</style>
